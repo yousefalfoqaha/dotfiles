2e2d42e2e91737e8f10e5ffa74af38d87a2573d2
yousefalfoqaha <yousefalfoqaha@proton.me>
2026-02-15 21:58:03 +0300

    update

--- init.lua
+++ init.lua
@@ -8,46 +8,32 @@ vim.o.relativenumber = true
 vim.diagnostic.config({ virtual_text = true })
 vim.o.winborder = 'rounded'
 
--- leader
+-- root namespace
 vim.g.mapleader = " "
 
--- fuzzy find
+-- find namespace
 vim.keymap.set('n', '<leader>ff', function() FzfLua.files() end)
 vim.keymap.set('n', '<leader>fb', function() FzfLua.buffers() end)
-vim.keymap.set('n', '<leader>fw', function() FzfLua.live_grep() end)
-vim.keymap.set('n', '<leader>fc', function() FzfLua.lgrep_curbuf() end)
+vim.keymap.set('n', '<leader>fw', function() FzfLua.lgrep_curbuf() end)
+vim.keymap.set('n', '<leader>fW', function() FzfLua.live_grep() end)
 vim.keymap.set('n', '<leader>fq', function() FzfLua.quickfix() end)
 vim.keymap.set('n', '<leader>fl', function() FzfLua.loclist() end)
 vim.keymap.set('n', '<leader>fs', function() FzfLua.lsp_document_symbols() end)
+vim.keymap.set('n', '<leader>fS', function() FzfLua.lsp_live_workspace_symbols() end)
 vim.keymap.set('n', '<leader>fr', function() FzfLua.lsp_references() end)
 
--- code
-vim.keymap.set('n', '<leader>cf', function() vim.lsp.buf.format() end)
+-- lsp namespace
+vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format() end)
 
 vim.pack.add({
+	{ src = "https://github.com/loctvl842/monokai-pro.nvim" },
 	{ src = "https://github.com/shaunsingh/nord.nvim" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/ibhagwan/fzf-lua" },
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(ev)
-		local client = vim.lsp.get_client_by_id(ev.data.client.id)
-		if client:supports_method('textDocument/completion') then
-			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-		end
-	end,
-})
-vim.cmd("set completeopt+=noselect")
-
 require("fzf-lua")
--- require("nvim-treesitter.config").setup({
--- 	ensure_installed = { "java" },
--- 	highlight = { enable = true },
--- 	install_dir = vim.fin.stdpath('data') .. '/site'
--- })
 
 vim.lsp.enable({ "lua_ls", "jdtls" })
 
-vim.cmd("colorscheme nord")
+vim.cmd("colorscheme monokai-pro")
---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,6 @@
+vim.pack.add({
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
+})
+
+require("nvim-treesitter.configs").setup({
+})
