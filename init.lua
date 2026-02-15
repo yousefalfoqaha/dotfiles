dd0623124b622f9082e4400d2e9b3147681a66c4
yousefalfoqaha <yousefalfoqaha@proton.me>
2026-02-15 16:18:31 +0300

    update

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 vim.o.termguicolors = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.o.scrolloff = 8
 vim.o.wrap = false
 vim.o.number = true
@@ -10,17 +10,21 @@ vim.o.winborder = 'rounded'
 
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>o', vim.cmd.so)
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>f', ':find **/')
-
-vim.keymap.set({'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
-vim.keymap.set({'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+vim.keymap.set('n', '<leader>cf', function()
+	vim.lsp.buf.format({ async = false })
+end)
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code actions" })
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Goo to definition" })
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = "Goo to references" })
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 
 vim.pack.add({
-  { src = "https://github.com/shaunsingh/nord.nvim" },
+	{ src = "https://github.com/shaunsingh/nord.nvim" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 })
 
-vim.lsp.enable({"lua_ls", "jdtls"})
+vim.lsp.enable({ "lua_ls", "jdtls" })
 vim.cmd("colorscheme nord")
