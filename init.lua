78a9a1c3d5756d6d41be02b43be837bcd61ccf9d
yousefalfoqaha <yousefalfoqaha@proton.me>
2026-02-15 23:33:10 +0300

    update

--- init.lua
+++ init.lua
@@ -16,8 +16,6 @@ vim.keymap.set('n', '<leader>ff', function() FzfLua.files() end)
 vim.keymap.set('n', '<leader>fb', function() FzfLua.buffers() end)
 vim.keymap.set('n', '<leader>fw', function() FzfLua.lgrep_curbuf() end)
 vim.keymap.set('n', '<leader>fW', function() FzfLua.live_grep() end)
-vim.keymap.set('n', '<leader>fq', function() FzfLua.quickfix() end)
-vim.keymap.set('n', '<leader>fl', function() FzfLua.loclist() end)
 vim.keymap.set('n', '<leader>fs', function() FzfLua.lsp_document_symbols() end)
 vim.keymap.set('n', '<leader>fS', function() FzfLua.lsp_live_workspace_symbols() end)
 vim.keymap.set('n', '<leader>fr', function() FzfLua.lsp_references() end)
@@ -25,14 +23,36 @@ vim.keymap.set('n', '<leader>fr', function() FzfLua.lsp_references() end)
 -- lsp namespace
 vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format() end)
 
+-- ergonomics
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<leader>bo', function()
+	local current = vim.api.nvim_get_current_buf()
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		if buf ~= current and vim.api.nvim_buf_is_loaded(buf) then
+			vim.api.nvim_buf_delete(buf, { force = true })
+		end
+	end
+end, { desc = "Delete all other buffers" })
+
+local github = function(x) return 'https://github.com/' .. x end
 vim.pack.add({
-	{ src = "https://github.com/loctvl842/monokai-pro.nvim" },
-	{ src = "https://github.com/shaunsingh/nord.nvim" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/ibhagwan/fzf-lua" },
+	{ src = github("nvim-tree/nvim-web-devicons") },
+	{ src = github("loctvl842/monokai-pro.nvim") },
+	{ src = github("shaunsingh/nord.nvim") },
+	{ src = github("neovim/nvim-lspconfig") },
+	{ src = github("ibhagwan/fzf-lua") },
 })
 
-require("fzf-lua")
+-- format on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+	pattern = '*',
+	callback = function()
+		local _, _ = pcall(vim.lsp.buf.format, { async = false })
+	end,
+})
+
+require('fzf-lua')
 
 vim.lsp.enable({ "lua_ls", "jdtls" })
 
--- lua/plugins/nvim-treesitter.lua
+++
@@ -1,6 +0,0 @@
-vim.pack.add({
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
-})
-
-require("nvim-treesitter.configs").setup({
-})
