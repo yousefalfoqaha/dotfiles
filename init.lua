fe0ab2452a3fc0d18b5ed366bfce61398e8b49ae
yousefalfoqaha <yousefalfoqaha@proton.me>
2026-02-15 14:46:53 +0300

    update

--- init.lua
+++ init.lua
@@ -10,27 +10,15 @@ vim.o.winborder = 'rounded'
 
 vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>o', vim.cmd.so)
-vim.keymap.set('n', '<leader>f', vim.cmd.pick.files)
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
 
 vim.keymap.set({'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
 vim.keymap.set({'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 
 vim.pack.add({
-  { src = "https://github.com/rose-pine/neovim" },
-	{ src = "https://github.com/nvim-mini/mini.pick"},
+  { src = "https://github.com/shaunsingh/nord.nvim" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 })
 
-require "mini.pick".setup()
-
-require("rose-pine").setup({
-  styles = {
-    bold = false,
-    italic = true,
-    transparency = true,
-  },
-})
-
 vim.lsp.enable({"lua_ls", "jdtls"})
-vim.cmd("colorscheme rose-pine")
+vim.cmd("colorscheme nord")
