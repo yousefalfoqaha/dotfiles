15dd4907cea713d28cb24d46fb4c35751e28d5c1
yousefalfoqaha <yousefalfoqaha@proton.me>
2026-08-12 15:31:45 +0300

    update

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,21 +1,21 @@
 local file = vim.fn.stdpath("data") .. "/theme"
 
-local groups = {
-	"Normal",
-	"NormalNC",
-	"SignColumn",
-	-- "EndOfBuffer",
-	-- "LineNr",
-	"CursorLineNr",
-	-- "FoldColumn",
-	-- "VertSplit",
-	-- "StatusLine",
-	-- "StatusLineNC",
-	-- "TabLine",
-	-- "TabLineFill",
-	-- "Pmenu",
-	-- "PmenuSel",
-}
+-- local groups = {
+-- 	"Normal",
+-- 	"NormalNC",
+-- 	"SignColumn",
+-- 	-- "EndOfBuffer",
+-- 	-- "LineNr",
+-- 	"CursorLineNr",
+-- 	-- "FoldColumn",
+-- 	-- "VertSplit",
+-- 	-- "StatusLine",
+-- 	-- "StatusLineNC",
+-- 	-- "TabLine",
+-- 	-- "TabLineFill",
+-- 	-- "Pmenu",
+-- 	-- "PmenuSel",
+-- }
 
 local f = io.open(file, "r")
 if f then
@@ -39,14 +39,14 @@ vim.api.nvim_create_user_command("Theme", function(opts)
 		end
 	end
 
-	for _, group in ipairs(groups) do
-		vim.api.nvim_set_hl(0, group, { bg = "none" })
-	end
+	-- for _, group in ipairs(groups) do
+	-- 	vim.api.nvim_set_hl(0, group, { bg = "none" })
+	-- end
 end, {
 	nargs = 1,
 	complete = "color",
 })
 
-for _, group in ipairs(groups) do
-	vim.api.nvim_set_hl(0, group, { bg = "none" })
-end
+-- for _, group in ipairs(groups) do
+-- 	vim.api.nvim_set_hl(0, group, { bg = "none" })
+-- end
