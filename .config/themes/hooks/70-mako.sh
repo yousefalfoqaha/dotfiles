d5f97eecb450f000ec922383c495f19785d7effb
Yousef Alfoqaha <y.alfoqaha@proton.me>
2026-08-21 19:01:43 +0300

    update

--- .config/themes/hooks/70-mako.sh
+++ .config/themes/hooks/70-mako.sh
@@ -5,33 +5,36 @@ source "$DOTFILES_FONT_HOME/$(cat "$DOTFILES_FONT_STATE").sh"
 
 cat << EOF > "$DOTFILES_THEME_STATE/mako"
 font=${FONT_BASE} ${FONT_BASE_SIZE}
-background-color=${B01}
+background-color=${B00}DD
 text-color=${B05}
-border-color=${B02}
+border-color=${B01}
 progress-color=${B0D}50
 
 border-size=1
 border-radius=0
 padding=4
-margin=8
+# Top, Right, Bottom, Left
+outer-margin=8,0,8,8
 
-anchor=top-right
+anchor=bottom-right
 default-timeout=5000
 ignore-timeout=0
 format=<b>%s</b>\n%b
 
 [urgency=critical]
-background-color=${B01}
-text-color=${B08}
+background-color=${B08}DD
+text-color=${B00}
 border-color=${B08}
 default-timeout=0
 
 [app-name=osd]
 layer=overlay
 history=0
-anchor=top-center
+anchor=bottom-center
 group-by=app-name
-format=<b>%s</b>\n%b
+format=<b>%s</b>
+outer-margin=8,0,8,8
+text-alignment=center
 
 [app-name=osd grouped=false]
 invisible=0
